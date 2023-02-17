459cfcf5c5a50044fdc1e5e5a410de9f6139c46b
emmaparis <elp.dance@gmail.com>
2023-02-16 22:37:57 -0500

    past,present, future

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,143 @@ $(document).ready(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
+    function checkTime() {
+
+        if (parseInt(box12.attr('data-hour')) > timeNow) {
+            box12.removeClass('past present future');
+            box12.addClass('future');
+  
+        } else if (parseInt(box12.attr('data-hour')) === timeNow) {
+            box12.removeClass('past present future');
+            box12.addClass('present');
+  
+  
+        } else {
+  
+            box12.removeClass('past present future');
+            box12.addClass('past');
+  
+        }
+  
+        if (parseInt(box1.attr('data-hour')) > timeNow) {
+            box1.removeClass('past present future');
+            box1.addClass('future')
+  
+        } else if (parseInt(box1.attr('data-hour')) === timeNow) {
+            box1.removeClass('past present future');
+            box1.addClass('present');
+  
+  
+        } else {
+  
+            box1.removeClass('past present future');
+            box1.addClass('past');
+  
+        }
+  
+        if (parseInt(box2.attr('data-hour')) > timeNow) {
+            box2.removeClass('past present future');
+            box2.addClass('future');
+  
+        } else if (parseInt(box2.attr('data-hour')) === timeNow) {
+            box2.removeClass('past present future');
+            box2.addClass('present');
+  
+  
+        } else {
+  
+            box2.removeClass('past present future');
+            box2.addClass('past');
+  
+        }
+  
+        if (parseInt(box3.attr('data-hour')) > timeNow) {
+            box3.removeClass('past present future');
+            box3.addClass('future');
+  
+        } else if (parseInt(box3.attr('data-hour')) === timeNow) {
+            box3.removeClass('past present future');
+            box3.addClass('present');
+  
+  
+        } else {
+            box3.removeClass('past present future');
+            box3.addClass('past');
+  
+        }
+  
+        if (parseInt(box4.attr('data-hour')) > timeNow) {
+            box4.removeClass('past present future');
+            box4.addClass('future');
+  
+        } else if (parseInt(box4.attr('data-hour')) === timeNow) {
+            box4.removeClass('past present future');
+            box4.addClass('present');
+  
+        } else {
+            box4.removeClass('past present future');
+            box4.addClass('past');
+  
+        }
+  
+        if (parseInt(box5.attr('data-hour')) > timeNow) {
+            box5.removeClass('past present future');
+            box5.addClass('future');
+        } else if (parseInt(box5.attr('data-hour')) === timeNow) {
+            box5.removeClass('past present future');
+            box5.addClass('present');
+  
+  
+        } else {
+            box5.removeClass('past present future');
+            box5.addClass('past');
+  
+        }
+  
+        if (parseInt(box9.attr('data-hour')) > timeNow) {
+            box9.removeClass('past present future');
+            box9.addClass('future');
+        } else if (parseInt(box9.attr('data-hour')) === timeNow) {
+            box9.removeClass('past present future');
+            box9.addClass('present');
+  
+  
+        } else {
+            box9.removeClass('past present future');
+            box9.addClass('past');
+  
+        }
+  
+        if (parseInt(box10.attr('data-hour')) > timeNow) {
+            box10.removeClass('past present future');
+            box10.addClass('future');
+        } else if (parseInt(box10.attr('data-hour')) === timeNow) {
+            box10.removeClass('past present future');
+            box10.addClass('present');
+  
+  
+        } else {
+            box10.removeClass('past present future');
+            box10.addClass('past');
+  
+        }
+  
+        if (parseInt(box11.attr('data-hour')) > timeNow) {
+            box11.removeClass('past present future');
+            box11.addClass('future');
+        } else if (parseInt(box11.attr('data-hour')) === timeNow) {
+            box11.removeClass('past present future');
+            box11.addClass('present');
+  
+  
+        } else {
+            box11.removeClass('past present future');
+            box11.addClass('past');
+  
+        }
+  
+    }
+  
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
